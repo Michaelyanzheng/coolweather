e90f0cd89a4dddfe1e9bc6ae2052b69425523c16
Zheng <854268426@qq.com>
2014-11-03 17:18:03 +0800

    新增第二次

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -248,6 +248,8 @@ public class ChooseAreaActivity extends Activity {
 	
 	/**
 	 * 捕获back按键，根据当前的级别来判断，此时应该应该返回市列表，省列表，还是直接退出
+	 * 
+	 * 哈哈太好了
 	 */
 	public void onBackPressed(){
 		if(currentLevel == LEVEL_COUNTRY){
